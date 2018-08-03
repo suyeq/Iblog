9da34ddcccda6339edf5e15156caf856e0fbdb72
suye <cangmingfeng@gmail.com>
2018-08-03 20:20:30 +0800

    blog was completed basicly

--- src/main/resources/static/js/userspace/blogedit.js
+++ src/main/resources/static/js/userspace/blogedit.js
@@ -18,76 +18,105 @@ $(function() {
         },
         resize:'vertical',
         localStorage:'md',
-        imgurl: 'http://localhost:8081',
-        base64url: 'http://localhost:8081'
+         imgurl: 'http://localhost:8080',
+         base64url: 'http://localhost:8080'
     });
  
 
     
     // 初始化下拉
     $('.form-control-chosen').chosen();
-    
-    
-    // 初始化标签
-    $('.form-control-tag').tagsInput({
-    	'defaultText':'输入标签'
- 
-    });
+    //
+    //
+    // // 初始化标签
+    $('.form-control-tag').val(
+    	'请输入标签'
+    );
  
  	$("#uploadImage").click(function() {
-		$.ajax({
-		    url: 'http://localhost:8081/upload',
-		    type: 'POST',
-		    cache: false,
-		    data: new FormData($('#uploadformid')[0]),
-		    processData: false,
-		    contentType: false,
-		    success: function(data){
-		    	var mdcontent=$("#md").val();
-		    	 $("#md").val(mdcontent + "\n![]("+data +") \n");
- 
-	         }
-		}).done(function(res) {
-			$('#file').val('');
-		}).fail(function(res) {});
+ 		console.log('daddad')
+		// $.ajax({
+		//     url: 'http://localhost:8081/upload',
+		//     type: 'POST',
+		//     cache: false,
+		//     data: new FormData($('#uploadformid')[0]),
+		//     processData: false,
+		//     contentType: false,
+		//     success: function(data){
+		//     	var mdcontent=$("#md").val();
+		//     	 $("#md").val(mdcontent + "\n![]("+data +") \n");
+        //
+	     //     }
+		// }).done(function(res) {
+		// 	$('#file').val('');
+		// }).fail(function(res) {});
  	})
  
  	// 发布博客
  	$("#submitBlog").click(function() {
- 		
-		// 获取 CSRF Token 
+		// 获取 CSRF Token
 		// var csrfToken = $("meta[name='_csrf']").attr("content");
 		// var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+        $.ajax({
+                    url: '/u/'+ $(this).attr("userName") + '/blogs/edit',
+                    type: 'POST',
+                    contentType: "application/json; charset=utf-8",
+                    data:JSON.stringify({"id":$('#blogId').val(),
+                        "title": $('#title').val(),
+                        "summary": $('#summary').val() ,
+                        "content": $('#md').val(),
+                        "catalog":{"id":$('#catalogSelect').val()},
+                        "tags":$('.form-control-tag').val()
+                    }),
+                    // beforeSend: function(request) {
+                    //     request.setRequestHeader(csrfHeader, csrfToken); // 添加  CSRF Token
+                    // },
+                    success: function(data){
+                        if (data.success) {
+                            // 成功后，重定向
+                            window.location = data.body;
 
-		console.log('jinlaile')
-		$.ajax({
-		    url: '/u/'+ $(this).attr("userName") + '/blogs/edit',
-		    type: 'POST',
-			contentType: "application/json; charset=utf-8",
-		    data:JSON.stringify({"id":$('#blogId').val(), 
-		    	"title": $('#title').val(), 
-		    	"summary": $('#summary').val() , 
-		    	"content": $('#md').val(), 
-		    	"catalog":{"id":$('#catalogSelect').val()},
-		    	"tags":$('.form-control-tag').val()
-		    	}),
-			// beforeSend: function(request) {
-			//     request.setRequestHeader(csrfHeader, csrfToken); // 添加  CSRF Token
-			// },
-			 success: function(data){
-				 if (data.success) {
-					// 成功后，重定向
-					 window.location = data.body;
-				 } else {
-					 toastr.error("error!"+data.message);
-				 }
-				 
-		     },
-		     error : function() {
-		    	 toastr.error("error!");
-		     }
-		})
+                        } else {
+                            toastr.error("error!"+data.message);
+                        }
+
+                    },
+                    error : function() {
+                        toastr.error("error!");
+                    }
+                })
  	})
+	
+    // function tab() {
+    //     console.log('jinlaile')
+    //     $.ajax({
+    //         url: '/u/'+ $(this).attr("userName") + '/blogs/edit',
+    //         type: 'POST',
+    //         contentType: "application/json; charset=utf-8",
+    //         data:JSON.stringify({"id":$('#blogId').val(),
+    //             "title": $('#title').val(),
+    //             "summary": $('#summary').val() ,
+    //             "content": $('#md').val(),
+    //             "catalog":{"id":$('#catalogSelect').val()},
+    //             "tags":$('.form-control-tag').val()
+    //         }),
+    //         // beforeSend: function(request) {
+    //         //     request.setRequestHeader(csrfHeader, csrfToken); // 添加  CSRF Token
+    //         // },
+    //         success: function(data){
+    //             if (data.success) {
+    //                 // 成功后，重定向
+    //                 window.location = data.body;
+    //             } else {
+    //                 toastr.error("error!"+data.message);
+    //             }
+    //
+    //         },
+    //         error : function() {
+    //             toastr.error("error!");
+    //         }
+    //     })
+    // }
  	
  	
 });
